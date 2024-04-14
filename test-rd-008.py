526544e1d1a06f4f2978e9d2f6abfd5c85065d9d
XXXYTKA <167037176+XXXYTKA@users.noreply.github.com>
2024-04-15 01:35:56 +0300

    Add files via upload

---
+++ test-rd-008.py
@@ -0,0 +1,26 @@
+x = 45
+print('Здраствуйте')
+if x < 5:
+    print('меньше пяти')
+print('всего хорошего')
+a, b = 4, 6
+if a > b:
+    print('a > b')
+if a < b:
+    print('a < b')
+if a < b and a >= 0:
+    print('кайф')
+if (a < b) and (b > 0 or a > 1000):
+    print('кайф')
+if a > 0 and b < 8:
+    print('кайф')
+if '34' > '124':
+    'кайф'
+if '123' > '21':
+    print('кайф')
+if 5 > '12':
+    print('кайф')
+if [4, 2] > 2:
+    print('кайф')
+if '6' != 5:
+    print('кайф')
